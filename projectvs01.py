947f837d9e75351558f5cc028fc8ca6930d5f414
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-02-07 11:15:29 +0700

    ..

---
+++ projectvs01.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.image('')
+st.header("การนําเสนอสถิติการเกิดอุบัติเหตุของประเทศไทย")
+
+coll,col2=st.columns(2)
+
+with coll:
+    st.subheader("จํานวนผู้เสียชีวิต")
+    st.write("2,5600")
+with col2:
+    st.subheader("จํานวนผู้เสียชีวิต")
+    st.write("2, 5600")
